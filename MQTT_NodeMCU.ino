54749407a6305da8215e4bb0edc0944103989a78
Sai Shibu N B <saishibu38380@gmail.com>
2020-02-17 17:15:22 +0530

    Update MQTT_NodeMCU.ino

--- MQTT_NodeMCU.ino
+++ MQTT_NodeMCU.ino
@@ -2,17 +2,22 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+//define variables to store ssid, password,broker address and port number
 char* ssid = "WIFISSID";
 char* password =  "PASSWORD";
-const char* mqttServer = "MQTT Host";
+const char* mqttBroker = "MQTT Host";
 const int mqttPort = 1883;
 
+//create a WiFi client that manages the WiFi connections
 WiFiClient espClient;
+
+//create a MQTT Client that handles MQTT Publish & Subscribe
 PubSubClient client(espClient);
 
 void setup() {
 
   Serial.begin(115200);
+  //Connect to WiFi
   WiFi.begin(ssid, password);
   Serial.println("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -24,7 +29,8 @@ void setup() {
 
   Serial.println("\nConnected to the WiFi network");
 
-  client.setServer(mqttServer, mqttPort);
+  //Connect to MQTT Broker
+  client.setServer(mqttBroker, mqttPort);
 
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
@@ -46,6 +52,7 @@ void setup() {
 }
 void loop() {
   //Serial.println(WiFi.status());
+  //Publish/subscribe to/from the broker once the client is connected
   if (client.connected()) {
   client.publish("TOPIC/Channel", "Hello");
   }
